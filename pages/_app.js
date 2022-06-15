d9cbbee3243ed4d89e9faba6bee2cc50a674a966
JMaria <jmariadelacruzsanchez@gmail.com>
2022-06-15 16:49:47 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
                 <meta name="description" content="NFT Marketplace" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+            <MoralisProvider initializeOnMount={false}>
                 <NotificationProvider>
                     <Header />
                     <Component {...pageProps} />
